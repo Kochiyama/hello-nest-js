054cd82ccaa9d05131bb3875b2d28226cc704869
Haruo Kochiyama <kochiyama.pro@gmail.com>
2021-11-28 13:42:22 -0400

    Refactor: move prisma service

--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Prisma, User } from '.prisma/client';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
