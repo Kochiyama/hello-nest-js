fe62d42bed965f0659a02f56c2f61ad70de0f3f4
Kochiyama <marcelokochiyama2210@gmail.com>
2021-11-30 16:24:49 -0400

    Chore: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import { logger } from './common/middlewares/logger.middleware';
 
 async function bootstrap() {
@@ -16,6 +17,16 @@ async function bootstrap() {
 
   // // Log every new request
   // app.use(logger);
+
+  // Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('Hello Nest')
+    .setDescription('This is the @Kochiyama nest test area')
+    .setVersion('1.0')
+    .addTag('hello-nest')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
